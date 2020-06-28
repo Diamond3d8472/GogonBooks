86f91b27696ce66eea482168d9d02f16f4e50c42
Diamond3d8472 <55988960+Diamond3d8472@users.noreply.github.com>
2020-06-28 13:36:55 -0300

    Alteração do titulo da pagina
    Alteração do titulo da pagina para teste

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-            <title>Gogonbooks</title>
+            <title>Gogon</title>
             <link rel="icon" type="imagem/ico" href="imagens/favicon.ico" />
 
             <meta name="viewport" content="width=device-width,initial-scale=1.0">
